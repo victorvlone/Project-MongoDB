a9b1baa4121b8a5483e04bb94172b1f0f0fe5532
João Victor <victorvlone999@gmail.com>
2025-10-01 17:33:12 -0300

    feat: listar posts do usuario

--- src/main/java/com/project/demo/config/Instantiation.java
+++ src/main/java/com/project/demo/config/Instantiation.java
@@ -31,13 +31,16 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, LocalDate.of(2015, 2, 3), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, LocalDate.of(2018, 12, 5), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
--- src/main/java/com/project/demo/entities/User.java
+++ src/main/java/com/project/demo/entities/User.java
@@ -1,6 +1,10 @@
 package com.project.demo.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -11,7 +15,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Document
 public class User {
 
@@ -20,6 +23,15 @@ public class User {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
     @Override
     public int hashCode() {
         final int prime = 31;
