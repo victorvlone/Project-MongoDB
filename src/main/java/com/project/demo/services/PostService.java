cef375a5b76364d58969a9a8b297312254a330e2
João Victor <victorvlone999@gmail.com>
2025-10-02 08:42:38 -0300

    implementar @query

--- src/main/java/com/project/demo/services/PostService.java
+++ src/main/java/com/project/demo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return postRepository.findByTitleContaining(text);
+        return postRepository.findByTitle(text);
     }
 }
